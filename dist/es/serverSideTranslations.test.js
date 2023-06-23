982990739070e7f0aaf7a994664b0b81a4b5bc8d
stefagr <grisendistefano@gmail.com>
2023-06-23 12:27:23 +0200

    add dist files

---
+++ dist/es/serverSideTranslations.test.js
@@ -0,0 +1,152 @@
+import fs from 'fs';
+import { serverSideTranslations } from './serverSideTranslations';
+jest.mock('fs', () => ({
+  existsSync: jest.fn(),
+  readdirSync: jest.fn()
+}));
+describe('serverSideTranslations', () => {
+  beforeEach(() => {
+    fs.existsSync.mockReturnValueOnce(false);
+    fs.existsSync.mockReturnValueOnce(true);
+    fs.readdirSync.mockReturnValue([]);
+  });
+  afterEach(jest.resetAllMocks);
+  it('throws if initialLocale is not passed', async () => {
+    await expect(serverSideTranslations(undefined)).rejects.toThrow('Initial locale argument was not passed into serverSideTranslations');
+  });
+  describe('When namespacesRequired is not provided', () => {
+    beforeEach(() => {
+      fs.readdirSync.mockImplementation(path => ['common', `namespace-of-${path.split('/').pop()}`]);
+    });
+    it('returns all namespaces', async () => {
+      const props = await serverSideTranslations('en-US', undefined, {
+        i18n: {
+          defaultLocale: 'en-US',
+          locales: ['en-US', 'fr-CA']
+        }
+      });
+      expect(fs.existsSync).toHaveBeenCalledTimes(0);
+      expect(fs.readdirSync).toHaveBeenCalledTimes(1);
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/en-US'));
+      expect(props._nextI18Next.initialI18nStore).toEqual({
+        'en-US': {
+          common: {},
+          'namespace-of-en-US': {}
+        }
+      });
+    });
+    it('returns all namespaces with fallbackLng (as string)', async () => {
+      const props = await serverSideTranslations('en-US', undefined, {
+        i18n: {
+          defaultLocale: 'fr-BE',
+          fallbackLng: 'fr',
+          locales: ['nl-BE', 'fr-BE']
+        }
+      });
+      expect(fs.readdirSync).toHaveBeenCalledTimes(2);
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/fr'));
+      expect(props._nextI18Next.initialI18nStore).toEqual({
+        'en-US': {
+          common: {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        },
+        fr: {
+          common: {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        }
+      });
+    });
+    it('returns all namespaces with fallbackLng (as array)', async () => {
+      const props = await serverSideTranslations('en-US', undefined, {
+        i18n: {
+          defaultLocale: 'en-US',
+          fallbackLng: ['en', 'fr'],
+          locales: ['en-US', 'fr-CA']
+        }
+      });
+      expect(fs.readdirSync).toHaveBeenCalledTimes(3);
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/en-US'));
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/en'));
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/fr'));
+      expect(props._nextI18Next.initialI18nStore).toEqual({
+        en: {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        },
+        'en-US': {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        },
+        fr: {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        }
+      });
+    });
+    it('returns all namespaces with fallbackLng (as object)', async () => {
+      const props = await serverSideTranslations('en-US', undefined, {
+        i18n: {
+          defaultLocale: 'nl-BE',
+          fallbackLng: {
+            default: ['fr'],
+            'nl-BE': ['en']
+          },
+          locales: ['nl-BE', 'fr-BE']
+        }
+      });
+      expect(fs.readdirSync).toHaveBeenCalledTimes(3);
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/en'));
+      expect(fs.readdirSync).toHaveBeenCalledWith(expect.stringMatching('/public/locales/fr'));
+      expect(props._nextI18Next.initialI18nStore).toEqual({
+        en: {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        },
+        'en-US': {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        },
+        fr: {
+          common: {},
+          'namespace-of-en': {},
+          'namespace-of-en-US': {},
+          'namespace-of-fr': {}
+        }
+      });
+    });
+  });
+  it('returns props', async () => {
+    const props = await serverSideTranslations('en-US', [], {
+      i18n: {
+        defaultLocale: 'en-US',
+        locales: ['en-US', 'fr-CA']
+      }
+    });
+    expect(props).toEqual({
+      _nextI18Next: {
+        initialI18nStore: {
+          'en-US': {}
+        },
+        initialLocale: 'en-US',
+        userConfig: {
+          i18n: {
+            defaultLocale: 'en-US',
+            locales: ['en-US', 'fr-CA']
+          }
+        }
+      }
+    });
+  });
+});
